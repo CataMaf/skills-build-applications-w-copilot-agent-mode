b61052da781dd0e997f17585f286497c012e91be
Maftei Catalin <139338234+CataMaf@users.noreply.github.com>
2025-06-18 13:14:10 +0000

    second commit bis bis

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django",
+        "ENGINE": "django", # Use 'django.db.backends.sqlite3' for SQLite or 'django.db.backends.postgresql' for PostgreSQL
         "NAME": "octofit_db",
     }
 }
